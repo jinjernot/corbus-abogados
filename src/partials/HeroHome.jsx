cfffae51fc7d54a9ebe67f01ba019bc720b98634
norindes <norindes@gmail.com>
2023-03-04 12:11:38 -0600

    colors

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -36,7 +36,7 @@ function HeroHome() {
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Corbus {/* <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400"> */}Abogados{/* </span> */}</h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Firma de abogados con más de 45 años de experiencia en el ámbito del Derecho Laboral, Seguridad Social y Amparo, ofreciendo siempre agilidad y calidad en el servicio de asesoramiento así como manejo del litigio individual y colectivo.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Firma de abogados con más de XXS años de experiencia en el ámbito del Derecho Laboral, Seguridad Social y Amparo, ofreciendo siempre agilidad y calidad en el servicio de asesoramiento así como manejo del litigio individual y colectivo.</p>
 {/*               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
